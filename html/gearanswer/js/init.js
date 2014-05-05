5d03a7f8279fe332085d39867f353d849979af53
winkidney <winkidney@gmail.com>
2014-05-05 16:06:01 +0800

    UPDATE for editor.

--- html/gearanswer/js/init.js
+++ html/gearanswer/js/init.js
@@ -1,3 +1,22 @@
+
+function createUEditor() {
+        UE.getEditor('editor-area');
+    }
+function deleteUEditor() {
+        UE.getEditor('editor-area').destroy();
+    }
+function createMDEditor() {
+    $("#editor-area").markdown({
+            callback: function(e){
+                // Replace selection with some drinks
+                $("#use-ueditor").change(e.showEditor())
+            }
+    });
+}
+function deleteMDEditor() {
+        
+}
+
 $(document).ready(function(){
     //create menu
 	$(".show-menu").bind("click",function () {  
@@ -6,6 +25,16 @@ $(document).ready(function(){
     $("#core-tabs").tabs(
         { show: { effect: "fadeIn", duration: 800 } }
     );
-     SyntaxHighlighter.all();
+    $( "#use-markdown" ).change(function(){
+        deleteUEditor();
+        createMDEditor();
+    });
+    $( "#use-ueditor" ).change(function(){
+        deleteMDEditor();
+        createUEditor();
+    });
+    
+    SyntaxHighlighter.all();
+    createUEditor();
 });
 
